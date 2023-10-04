7a1ed7bbe3ea5464c4d8ec4ec24f9b2c6abcdd1d
Jettinger101 <91003684+Jettinger101@users.noreply.github.com>
2023-10-04 09:11:10 +0100

    Update and rename Notts_EC.sql to EC.sql

--- EC.sql
+++ EC.sql
@@ -10,10 +10,10 @@ USE NHSE_SUSPlus_Live
 ---- SNOMED CODES -----
 
 --- used table to look at coding across all of Northampton ---
-DROP TABLE IF EXISTS NHSE_Sandbox_HEU.temp.COPD_snomed 
-CREATE TABLE NHSE_Sandbox_HEU.temp.COPD_snomed 
+DROP TABLE IF EXISTS NHSE_Sandbox.temp.COPD_snomed 
+CREATE TABLE NHSE_Sandbox.temp.COPD_snomed 
 (snomed_ct NVARCHAR(20) NULL)
-INSERT INTO NHSE_Sandbox_HEU.temp.COPD_snomed 
+INSERT INTO NHSE_Sandbox.temp.COPD_snomed 
 VALUES 
 -- COPD https://snomedbrowser.com/Codes/Details/13645005 -- 
 ('195951007'),('13645005'),('47938003'), ('106001000119101'), ('196001008') ,('135836000'),('313296004'), ('313297008')
@@ -33,10 +33,10 @@ left join [dbo].[tbl_Data_SUS_EC_2122_Cost]as cost
 on cost.EC_Ident = ec.EC_Ident
 left join [dbo].[tbl_Data_SUS_EC_Diagnosis] as diag
 on diag.EC_Ident = ec.EC_Ident
-WHERE  diag.EC_Diagnosis_01 IN (select snomed_ct from NHSE_Sandbox_HEU.temp.COPD_snomed) OR 
-diag.EC_Diagnosis_02 IN (select snomed_ct from NHSE_Sandbox_HEU.temp.COPD_snomed) AND
+WHERE  diag.EC_Diagnosis_01 IN (select snomed_ct from NHSE_Sandbox.temp.COPD_snomed) OR 
+diag.EC_Diagnosis_02 IN (select snomed_ct from NHSE_Sandbox.temp.COPD_snomed) AND
 ec.Der_Financial_Year = '2021/22' AND
-GP_Practice_Code IN (SELECT GP_code FROM NHSE_Sandbox_HEU.temp.HEU_097_Notts_GP
+GP_Practice_Code IN (SELECT GP_code FROM NHSE_Sandbox.temp.GP_List
 group by diag.EC_Diagnosis_01
 
 /* main records for COPD and AECOPD - therefore are taking these forward to final analysis */
@@ -45,7 +45,7 @@ group by diag.EC_Diagnosis_01
 
 ---FINAL CODE ---
 
-DROP TABLE IF EXISTS NHSE_Sandbox_HEU.temp.Notts_EC
+DROP TABLE IF EXISTS NHSE_Sandbox.temp.EC
 SELECT 
 cost.Tariff_Total_Payment,
 ec.ec_ident,
@@ -53,26 +53,31 @@ diag.EC_Diagnosis_01,
 GP.ICP_Name,
 Der_Provider_Code,
 EC.Der_Provider_Site_Code
-INTO NHSE_Sandbox_HEU.temp.Notts_EC
+INTO NHSE_Sandbox.temp.EC
 FROM [dbo].[tbl_Data_SUS_EC] as ec
 left join [dbo].[tbl_Data_SUS_EC_2122_Cost]as cost
 on cost.EC_Ident = ec.EC_Ident 
 left join [dbo].[tbl_Data_SUS_EC_Diagnosis] as diag
 on diag.EC_Ident = ec.EC_Ident
-left join NHSE_Sandbox_HEU.temp.HEU_097_Notts_GP as gp
+left join NHSE_Sandbox.temp.GP_List as gp
 ON ec.GP_Practice_Code = gp.GP_code
 WHERE (diag.EC_Diagnosis_01 = '13645005' OR diag.EC_Diagnosis_01 = '195951007') AND
 --diag.EC_Diagnosis_02 = '13645005' OR diag.EC_Diagnosis_02 = '195951007' AND
 ec.Der_Financial_Year = '202122' AND
-ec.GP_Practice_Code IN (SELECT GP_code FROM NHSE_Sandbox_HEU.temp.HEU_097_Notts_GP)
+ec.GP_Practice_Code IN (SELECT GP_code FROM NHSE_Sandbox.temp.GP_List)
 
 
 select 
 	count (DISTINCT ec_ident),
 	sum(Tariff_Total_Payment),
 	ICP_Name
-from NHSE_Sandbox_HEU.temp.Notts_EC
+from NHSE_Sandbox.temp.EC
 Group by ICP_Name
+
+
+
+
+
 
 
 
