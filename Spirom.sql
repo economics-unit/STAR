42ef8e51591a833e364f29764fb07aa22b09e930
Jettinger101 <91003684+Jettinger101@users.noreply.github.com>
2023-10-04 09:04:28 +0100

    Update and rename Notts Spriom.sql to Spirom.sql

--- Spirom.sql
+++ Spirom.sql
@@ -1,4 +1,4 @@
--- getting secondary care spirometry for Notts ---
+-- getting secondary care spirometry for xxx ---
 
 
 
@@ -13,14 +13,14 @@ SELECT COUNT (DISTINCT OPA.OPA_Ident),
 	sum(Tariff_Total_Payment)
 	--,Der_Provider_Code
 	FROM [dbo].[tbl_Data_SEM_OPA] as OPA
-	LEFT JOIN NHSE_Sandbox_HEU.temp.HEU_097_Notts_GP as GP
+	LEFT JOIN NHSE_Sandbox.temp.GP_List as GP
 	ON OPA.GP_Practice_Code = GP.GP_code
 	LEFT JOIN  [dbo].[tbl_Data_SEM_OPA_2122_Cost] as cost 
 	ON cost.OPA_Ident = OPA.OPA_Ident
 	WHERE
 		OPA.Der_Financial_Year = '2021/22' AND
 		Der_Procedure_All LIKE '%E932%' AND
-		GP_Practice_Code IN (SELECT GP_code FROM NHSE_Sandbox_HEU.temp.HEU_097_Notts_GP)
+		GP_Practice_Code IN (SELECT GP_code FROM NHSE_Sandbox.temp.GP_List)
 		group by Der_Attendance_Type, GP.ICP_Name
 
 --- AVERAGE COST ----
@@ -31,7 +31,7 @@ SELECT COUNT (DISTINCT OPA.OPA_Ident),
 	sum(Tariff_Total_Payment)
 	--,Der_Provider_Code
 	FROM [dbo].[tbl_Data_SEM_OPA] as OPA
-	LEFT JOIN NHSE_Sandbox_HEU.temp.HEU_097_Notts_GP as GP
+	LEFT JOIN NHSE_Sandbox.temp.GP_List as GP
 	ON OPA.GP_Code = GP.GP_code
 	LEFT JOIN  [dbo].[tbl_Data_SEM_OPA_2122_Cost] as cost 
 	ON cost.OPA_Ident = OPA.OPA_Ident
@@ -39,7 +39,7 @@ SELECT COUNT (DISTINCT OPA.OPA_Ident),
 		OPA.Der_Financial_Year = '2021/22' AND
 		Der_Procedure_All LIKE '%E932%' AND
 		Tariff_Total_Payment IS NOT NULL 
-		GP_Practice_Code IN (SELECT GP_code FROM NHSE_Sandbox_HEU.temp.HEU_097_Notts_GP)
+		GP_Practice_Code IN (SELECT GP_code FROM NHSE_Sandbox.temp.GP_List)
 		group by Der_Attendance_Type, ICP_Name
 
 
